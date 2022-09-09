2f1d822af9fb4b2464302c2ab50b592521142ea7
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2022-09-09 15:17:25 +0300

    Main page done
    [SAVE]

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+/*const xScrollElement = document.getElementById("x-sroll");
+
+xScrollElement.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    xScrollElement.scrollBy(evt.deltaY, 0)
+});*/
+
+//document.dispatchEvent(new KeyboardEvent('keydown', {'key': 'Shift'}))
