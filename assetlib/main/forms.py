213e5c868912da1ab90904ad39a53f5a1d5b42dd
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2023-04-01 09:52:28 +0300

    Finish login logic

---
+++ assetlib/main/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(min_length=3, max_length=30, initial="")
+    password = forms.CharField(min_length=8, max_length=100, initial="")
--- assetlib/main/views.py
+++ assetlib/main/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.http import HttpRequest
-
+from .forms import *
 # Create your views here.
+
 def login_view(request: HttpRequest):
 
-    return render(request, "main/login.html")
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+
+                return redirect("/index")
+            
+        return render(request, "main/login.html", { 'form': form, 'error_during_login': True })
+                
+    else:
+
+        return render(request, "main/login.html", { 'form': LoginForm()})
+
+    
 
 def index_view(request: HttpRequest):
 
