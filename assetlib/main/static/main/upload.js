c2ff9a147a97adb2685b025e54b8c8356270c250
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2023-04-08 13:08:44 +0300

    Upload page frontend finish

--- assetlib/main/static/main/upload.js
+++ assetlib/main/static/main/upload.js
@@ -1,20 +1,20 @@
 function updatePreviewModel(el) 
 {
-
-    if (FileReader && el.files && files.length) 
-    {
-        var fr = new FileReader();
-
-        fr.onload = function () 
-        {
-            document.getElementById("preview").src = fr.result;
-        }
-
-        fr.readAsDataURL(el.files[0]);
-    }
+    document.getElementById("preview").src = URL.createObjectURL(el.files[0]);
 }
 
 function updatePreviewTexture(el) 
 {
+    let preview = document.getElementById("texturesPreview");
+    preview.innerHTML = "";
 
+    const template = document.querySelector('#textureContainer');
+
+    for (const file of el.files)
+    {
+        let newContainer = template.content.cloneNode(true);
+        newContainer.querySelector('img').src = URL.createObjectURL(file);
+        newContainer.querySelector('h6').textContent = file.name;
+        preview.appendChild(newContainer);
+    }
 }
