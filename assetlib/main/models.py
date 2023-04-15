f70a6cb1c57d9bbe7abf420a827a062cc39516a4
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2023-04-15 22:11:34 +0300

    New version of data base (pls help)

--- assetlib/main/models.py
+++ assetlib/main/models.py
@@ -1,3 +1,5 @@
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 from django.db import models
 
 class CardTagsCategoriesModel(models.Model):
@@ -7,3 +9,24 @@ class CardTagsCategoriesModel(models.Model):
 class CardTagsModel(models.Model):
     tag_name = models.CharField(max_length=50)
     category = models.ForeignKey(CardTagsCategoriesModel, on_delete=models.CASCADE)
+
+class CardContentModel(models.Model):
+
+    def mesh_content_path(instance, filename):
+        return 'assets/{0}_{1}/{2}'.format(instance.card_name, instance.id, filename)
+
+    def texture_content_path(instance, filename):
+        return 'assets/{0}_{1}/textures/{2}'.format(instance.card_name, instance.id, filename)
+
+    card_name = models.TextField()
+    card_description = models.TextField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    blender_mesh = models.FileField(upload_to=mesh_content_path)
+    fbx_mesh = models.FileField(upload_to=mesh_content_path)
+    preview_mesh = models.FileField(upload_to=mesh_content_path)
+
+    tags = ArrayField(models.ForeignKey(CardTagsModel, on_delete=models.CASCADE))
+
+    textures = ArrayField(models.FileField(upload_to=texture_content_path))
