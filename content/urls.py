5fadd704ba45a3842f8a76dfdd0a1028421782c8
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2023-07-05 16:24:24 +0300

    Full asset download as ZIP instead blender only

--- content/urls.py
+++ content/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("upload/", views.upload_view, name='upload'),
     path("view/<int:asset_pk>", views.view_view, name='asset_view'),
+    path("download_asset/<int:asset_pk>", views.download_asset_zip, name='download_asset'),
     path("download_textures/<int:asset_pk>", views.download_textures_zip, name='download_textures')
 ]
--- content/views.py
+++ content/views.py
@@ -71,6 +71,41 @@ def upload_view(request: HttpRequest):
     return render(request, "content/upload.html", { 'form': UploadForm(), 'filters': AssetTagCategory.get_dictionary() })
 
 @login_required(login_url='/auth/', redirect_field_name=None)
+def download_asset_zip(request: HttpRequest, asset_pk):
+
+    asset = Asset.objects.filter(pk=asset_pk)
+
+    if not asset.exists():
+        return Http404()
+    
+    asset = asset.first()
+
+    if not request.user.has_perm("content.see_own") and asset.author == request.user:
+        return Http404()
+    
+    if not request.user.has_perm("content.see_others") and asset.author != request.user:
+        return Http404()
+    
+    zip_filename = "{}_asset.zip".format(asset.name)
+
+    bytes = io.BytesIO()
+
+    zipFile = zipfile.ZipFile(bytes, "w")
+
+    for file in (asset.blender_mesh.path, asset.fbx_mesh.path):
+        fdir, fname = os.path.split(file)
+        zipFile.write(file, fname)
+
+    for texture in map(lambda el: el.texture.path, asset.textures.all()):
+        fdir, fname = os.path.split(texture)
+        fname = os.path.join("textures", fname)
+        zipFile.write(texture, fname)
+
+    zipFile.close()
+
+    return HttpResponse(bytes.getvalue(), content_type = "application/x-zip-compressed", headers = { 'Content-Disposition': 'attachment; filename={}'.format(zip_filename) })
+
+@login_required(login_url='/auth/', redirect_field_name=None)
 def download_textures_zip(request: HttpRequest, asset_pk):
 
     asset = Asset.objects.filter(pk=asset_pk)
