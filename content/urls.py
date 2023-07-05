6883ec680be61b6d01aeb4f331a6a43088cb8d22
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2023-07-05 15:11:58 +0300

    Textures download in ZIP

--- content/urls.py
+++ content/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("upload/", views.upload_view, name='upload'),
     path("view/<int:asset_pk>", views.view_view, name='asset_view'),
+    path("download_textures/<int:asset_pk>", views.download_textures_zip, name='download_textures')
 ]
--- content/views.py
+++ content/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse, Http404
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import *
 from .forms import *
+
+import os
+import zipfile
+import io
 
 @login_required(login_url='/auth/', redirect_field_name=None)
 def view_view(request: HttpRequest, asset_pk):
@@ -19,8 +23,6 @@ def view_view(request: HttpRequest, asset_pk):
     
     if not request.user.has_perm("content.see_others") and asset.author != request.user:
         return redirect('library')
-    
-
 
     return render(request, "content/view.html", { 'asset': asset })
 
@@ -67,3 +69,33 @@ def upload_view(request: HttpRequest):
         return render(request, "content/upload.html", { 'form': form, 'filters': AssetTagCategory.get_dictionary() })
 
     return render(request, "content/upload.html", { 'form': UploadForm(), 'filters': AssetTagCategory.get_dictionary() })
+
+@login_required(login_url='/auth/', redirect_field_name=None)
+def download_textures_zip(request: HttpRequest, asset_pk):
+
+    asset = Asset.objects.filter(pk=asset_pk)
+
+    if not asset.exists():
+        return Http404()
+    
+    asset = asset.first()
+
+    zip_subdir = "textures"
+    zip_filename = "%s.zip" % zip_subdir
+
+    s = io.BytesIO()
+
+    zf = zipfile.ZipFile(s, "w")
+
+    for texture in map(lambda el: el.texture.path, asset.textures.all()):
+        fdir, fname = os.path.split(texture)
+        zip_path = os.path.join(zip_subdir, fname)
+        zf.write(texture, zip_path)
+
+    zf.close()
+
+    response = HttpResponse(s.getvalue(), content_type = "application/x-zip-compressed", headers = { 'Content-Disposition': 'attachment; filename=%s' % zip_filename})
+
+    #response['Content-Disposition'] = 'attachment; filename=%s' % zip_filename
+
+    return HttpResponse(s.getvalue(), content_type = "application/x-zip-compressed", headers = { 'Content-Disposition': 'attachment; filename=%s' % zip_filename})
