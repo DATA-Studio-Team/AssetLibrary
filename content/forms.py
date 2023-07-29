6eb585b16fe96296368100911198bb32875d06ad
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2023-07-29 21:25:09 +0300

    Forms limits fix

--- content/forms.py
+++ content/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class UploadForm(forms.Form):
-    card_name = forms.CharField(min_length=3, max_length=50, initial="")
-    card_description = forms.CharField(min_length=0, max_length=500, initial="")
+    card_name = forms.CharField(initial="")
+    card_description = forms.CharField(initial="")
 
     blender_mesh = forms.FileField(required=False)
     fbx_mesh = forms.FileField(required=False)
