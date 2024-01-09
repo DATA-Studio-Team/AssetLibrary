a288ac7ab055e4685a10ee5d6b9e4766330c3abd
CubeRuben <33936898+CubeRuben@users.noreply.github.com>
2024-01-09 22:55:13 +0300

    Update settings.py

--- assetlib/settings.py
+++ assetlib/settings.py
@@ -132,3 +132,6 @@ MEDIA_ROOT = 'files/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 524_288_000
+FILE_UPLOAD_MAX_MEMORY_SIZE = 524_288_000
